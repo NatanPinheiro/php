965012866ed994ce1b6d6098746d8890b5bb16c0
NatanPinheiro <pnatanpinheiro@gmail.com>
2019-01-17 00:37:25 -0200

    agora sim atualizei o arquivo com a resolução do problema de conflito

--- index.php
+++ index.php
@@ -1,12 +1,7 @@
 <?php 
-<<<<<<< HEAD
+
     echo "Olá Mundo! 456";
 
     echo "E aí gurizada!";
-=======
-    echo "Olá Mundo 123";
 
-    
-    echo "Olá Natan";
->>>>>>> cd59590c279cc99e21340b7e845b69bbf8e22e57
 ?>
